f5968f7b141be88533d9073871322157171caf78
mahmody <42693632+mahmody@users.noreply.github.com>
2019-01-21 14:03:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("By iiFireKingYTii_#7310")
+                    .setFooter("! ℳ𝒜ℋℳ𝒪𝒰𝒟 ♕♕💎♕♕#0007")
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
